b8907b7ba20140d1be1879d6e83a94e891accf4f
Aditya-2898261 <adithyat14072005@gmail.com>
2026-08-12 10:14:01 +0530

    Connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+main()
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((err) => {
+    console.error("MongoDB connection failde:", error);
+});
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI);
+}
 
 app.get("/",(req,res) => {
     res.send("URL SHORTENER API is running");
